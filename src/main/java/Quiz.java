0298ee5e2bcff2164d881f43245599e1fc5221ad
Leedonguk <donguk0105@naver.com>
2023-03-15 09:52:00 +0900

    별로 삼각형 만들기(왼쪽에 붙은)

--- src/main/java/Quiz.java
+++ src/main/java/Quiz.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Quiz {
+public class Quiz {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j < i; j++) {
+
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = 4; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
 }
