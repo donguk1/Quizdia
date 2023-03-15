f258b8826a86d3dab63ace193472b23c86ae94e2
Leedonguk <donguk0105@naver.com>
2023-03-16 08:48:41 +0900

    교재 88쪽 실습 3-1

--- src/main/java/Ex3_01.java
+++
@@ -1,8 +0,0 @@
-public class Ex3_01 {
-    public static void main(String[] args) {
-        System.out.printf("100 + 100");
-        System.out.printf("\n");
-        System.out.printf("%d", 100 + 100);
-        System.out.printf("\n");
-    }
-}
