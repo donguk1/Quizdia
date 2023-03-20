ec6f6b28b32f10e51e6d195f3db016d31d021a51
Leedonguk <donguk0105@naver.com>
2023-03-20 19:35:11 +0900

    교재 실습

---
+++ src/main/java/BJ/Bj1.java
@@ -0,0 +1,16 @@
+package BJ;
+
+import java.util.Scanner;
+
+public class Bj1{
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a, b;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        System.out.println(a + b);
+        }
+}
--- src/main/java/Quizdia.java
+++ src/main/java/Quizdia.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Quizdia {
+public class Quizdia {
+    public static void main(String[] args) {
+
+        int row = 5;
+        for(int i = 0; i < row; i++) {
+            for(int j=0; j < row - 1 - i; j++) {
+                System.out.print(" ");
+
+            }
+            for(int j = 0; j < 2*i+1; j++) {
+                System.out.print("*");
+            }
+        System.out.println();
+        }
+        for(int i = row - 1; i > 0; i--) {
+            for(int j = 0; j < row - i; j++) {
+                System.out.print("");
+            }
+            for(int j = i*2-1; j > 0; j--) {
+                System.out.print("*");
+            }
+        }
+    System.out.println();
+    }
 }
+
