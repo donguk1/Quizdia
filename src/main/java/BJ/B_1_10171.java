6e7bbb0e81e19bc09d4ead209499abd49ddac9f6
Leedonguk <donguk0105@naver.com>
2023-03-24 19:31:47 +0900

    교제 문제

---
+++ src/main/java/BJ/B_1_10171.java
@@ -0,0 +1,10 @@
+package BJ;
+
+public class B_1_10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
