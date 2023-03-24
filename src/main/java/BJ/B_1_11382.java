d6bd41e72a54446f5b4b09a4947202bcbb0639cf
Leedonguk <donguk0105@naver.com>
2023-03-24 19:25:17 +0900

    교제 문제

---
+++ src/main/java/BJ/B_1_11382.java
@@ -0,0 +1,15 @@
+package BJ;
+
+import java.util.Scanner;
+
+public class B_1_11382 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int c = s.nextInt();
+
+        System.out.println(a + b + c);
+    }
+}
