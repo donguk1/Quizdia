a5faa0512d4bfda87f668a3b8e8a54685141ab79
Leedonguk <donguk0105@naver.com>
2023-03-24 19:56:12 +0900

    백준 문제

---
+++ src/main/java/BJ/B_2_9498.java
@@ -0,0 +1,22 @@
+package BJ;
+
+import java.util.Scanner;
+
+public class B_2_9498 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+
+        if (a >= 90 && a <= 100) {
+            System.out.println("A");
+        } else if (a >= 80 && a <= 89) {
+            System.out.println("B");
+        } else if (a >= 70 && a <= 79) {
+            System.out.println("C");
+        } else if (a >= 60 && a <= 69) {
+            System.out.println("D");
+        } else if (a <= 59) {
+            System.out.println("F");
+        }
+    }
+}
