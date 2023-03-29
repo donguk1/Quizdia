7c06fb7db182f0911f27408ed7aa8b3b6691eade
Leedonguk <donguk0105@naver.com>
2023-03-29 10:55:59 +0900

    교재 문제

---
+++ src/main/java/Ex_6/Ex6_8.java
@@ -0,0 +1,13 @@
+package Ex_6;
+
+public class Ex6_8 {
+    public static void main(String[] args) {
+        int hap = 0;
+        int i;
+
+        for (i = 1; i <= 10; i++) {
+            hap += i;
+        }
+        System.out.printf("1에서 10까지의 합 : %d \n", hap);
+    }
+}
