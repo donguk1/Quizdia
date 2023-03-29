fa71c3b4c210e7e79c7bb7a84c8e46b2d268668f
Leedonguk <donguk0105@naver.com>
2023-03-29 10:28:49 +0900

    교재 문제

---
+++ src/main/java/Ex_6/Ex6_02.java
@@ -0,0 +1,12 @@
+package Ex_6;
+
+public class Ex6_02 {
+    public static void main(String[] args) {
+        int i;
+
+        for (i = 0; i < 5; i++)
+        {
+            System.out.printf("안녕하세요? 빙글빙글 for 문을 공부중입니다.^^\n");
+        }
+    }
+}
