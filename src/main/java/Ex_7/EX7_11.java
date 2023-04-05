8d9c73c7b5c24bdcbf0f610bc330ff49f7c3746a
Leedonguk <donguk0105@naver.com>
2023-04-05 10:44:23 +0900

    230405

---
+++ src/main/java/Ex_7/EX7_11.java
@@ -0,0 +1,19 @@
+package Ex_7;
+
+public class EX7_11 {
+    public static void main(String[] args) {
+        int hap = 0, i;
+
+        for( ; ; ) {
+            for (i = 1; i <= 100; i++) {
+                hap += i;
+                if(hap > 2000) {
+                    System.out.printf("%d \n", hap);
+                    hap = 0;
+                    break;
+                }
+            }
+            System.out.print("아직도 반복중...\n");
+        }
+    }
+}
