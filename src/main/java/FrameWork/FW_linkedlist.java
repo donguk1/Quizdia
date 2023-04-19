b43b98593f1a5abc47c63b171cf5ab1fd22b4c03
Leedonguk <donguk0105@naver.com>
2023-04-19 18:40:23 +0900

    23.04.18

---
+++ src/main/java/FrameWork/FW_linkedlist.java
@@ -0,0 +1,10 @@
+package FrameWork;
+
+public class FW_linkedlist {
+    public static void main(String[] args) {
+
+//        LinkedList 사용 이유
+//        중간 삽이이 편함
+//        asc, desc 가능(arraylist 는 asc만 가능)
+    }
+}
---
+++ src/main/java/FrameWork/FW_listmap.java
@@ -0,0 +1,8 @@
+package FrameWork;
+
+public class FW_listmap {
+    public static void main(String[] args) {
+//        db 한줄 단위 처리
+//        list add 는 열 단위
+    }
+}
